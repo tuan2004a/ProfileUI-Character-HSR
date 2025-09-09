042fd51ef439e8dc1fdb056ab6fcdea9538407a4
tuan2004a <bi03112004@gmail.com>
2025-09-09 22:30:51 +0700

    feat: update character assets and descriptions for Psyche and others

--- components/CharacterOverview/ArtWorks.tsx
+++ components/CharacterOverview/ArtWorks.tsx
@@ -5,10 +5,10 @@ const ArtWorks = () => {
 		<div className="grid grid-cols-2 gap-4 h-full">
 			<h1 className="col-span-2 text-2xl !font-semibold ">Art Words</h1>
 			<div className=" text-center rounded-3xl h-60 w-45 overflow-hidden  border border-white">
-				<img className="size-full object-cover" src="/img/Character/Berenica.webp" alt="" />
+				<img className="size-full object-cover" src="/img/artWord/ArtPsyche1.jpg" alt="" />
 			</div>
 			<div className=" text-center rounded-3xl h-60 w-45 overflow-hidden  border border-white">
-				<img className="size-full object-cover" src="/img/Character/Berenica.webp" alt="" />
+				<img className="size-full object-cover" src="/img/artWord/ArtPsyche3.jpg" alt="" />
 			</div>
 			<button className="col-span-2 bg-white rounded-2xl text-lg py-2 !font-semibold">View More</button>
 		</div>
--- components/CharacterOverview/SLice.tsx
+++ components/CharacterOverview/SLice.tsx
@@ -5,12 +5,20 @@ import { FaLocationArrow } from "react-icons/fa";
 
 const SLice = () => {
     return (
-		<div className="size-full h-75 rounded-4xl shadow-lg relative bg-cover bg-center" style={{ backgroundImage: `url("/img/themeChar/bg-Berenica.png")` }}>
-			<img className="absolute right-0 -translate-x-10 h-full object-contain object-center brightness-75" src="/img/bg-dark.png" alt="" />
-			<img className="absolute right-0 h-full -translate-y-9.5 -translate-x-13 scale-125" src="/img/bgChar/Berenica.png" alt="" />
+		<div className="size-full h-75 rounded-4xl shadow-lg relative bg-cover bg-center" style={{ backgroundImage: `url("/img/banner/bg-Psyche.png")` }}>
+			{/* bg-dark */}
+			<img className="absolute right-15 h-full object-contain object-center brightness-75" src="/img/bg-dark.png" alt="" />
+
+			{/* char */}
+			<img className="absolute right-20 h-full -translate-y-5.5 scale-115" src="/img/character/Berenica.png" alt="" />
+
+			{/* deco */}
+			<img className="absolute h-4/5 right-2 top-1/2 -translate-y-1/2" src="/img/deco01.png" alt="" />
+
+			{/* information */}
 			<div className="absolute left-25 top-1/2 -translate-y-1/2">
-				<h2 className="text-5xl leading-none">Berenica</h2>
-				<p className="max-w-sm font-medium pt-5 pb-3.5">"Wishes? I... can't think of any. Your smile in the morning is already the greatest pick-me-up of all."</p>
+				<h2 className="text-5xl leading-none">Psyche</h2>
+				<p className="max-w-sm font-medium pt-5 pb-3.5">"The courtyard at the Hypnos' House looked a bit barren with just a few trees, so I planted some white flowers there."</p>
 				<button className="flex items-center gap-1 px-0.5 !font-semibold relative after:absolute after:bottom-0 after:left-0  after:h-0.5 after:w-[60%] after:bg-black hover:after:w-full after:transition-all after:duration-300">
 					Read More
 					<FaLocationArrow />
